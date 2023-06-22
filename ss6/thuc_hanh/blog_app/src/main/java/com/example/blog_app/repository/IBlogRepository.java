d016775c26619e2da776b137bfa6cbb443af5dae
americanoc11 <hhd0511469699@gmail.com>
2023-06-22 20:55:54 +0700

    update

--- ss6/thuc_hanh/blog_app/src/main/java/com/example/blog_app/repository/IBlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blog_app.repository;
-
-import com.example.blog_app.model.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IBlogRepository extends JpaRepository<Blog,Integer> {
-}
